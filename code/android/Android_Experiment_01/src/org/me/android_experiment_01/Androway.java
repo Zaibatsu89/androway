213a298549fbe83aff8df5eb1c2f37bede961f0a
tymensteur@gmail.com <tymensteur@gmail.com@09e49e98-eaa8-4320-d9ac-a419819e7ac0>
2011-02-02 14:22:37 +0000

    Made changes. Wallpaper is showing + dark overlay. Also the app statusbar is showing in the bottom.

--- code/android/Android_Experiment_01/src/org/me/android_experiment_01/Androway.java
+++ code/android/Android_Experiment_01/src/org/me/android_experiment_01/Androway.java
@@ -1,12 +1,7 @@
 package org.me.android_experiment_01;
 
 import android.app.Activity;
-import android.app.WallpaperManager;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.widget.ImageView;
 
 /**
  *
@@ -18,18 +13,8 @@ public class Androway extends Activity {
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        // ToDo add your GUI initialization code here
-		// See res/layout/wallpaper_2.xml for this
-        // view layout definition, which is being set here as
+        
         // the content of our screen.
         setContentView(R.layout.main);
-        final WallpaperManager wallpaperManager = WallpaperManager.getInstance(this);
-        final Drawable wallpaperDrawable = wallpaperManager.getDrawable();
-        wallpaperDrawable.setColorFilter(Color.GRAY, PorterDuff.Mode.MULTIPLY);
-
-        final ImageView imageView = (ImageView) findViewById(R.id.imageview);
-		imageView.setScaleType(ImageView.ScaleType.CENTER);
-        imageView.setDrawingCacheEnabled(true);
-        imageView.setImageDrawable(wallpaperDrawable);
     }
 }
