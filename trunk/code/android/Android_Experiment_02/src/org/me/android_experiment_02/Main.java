02a5142a05fd1320b89e2daf725838a9ed26654d
tymensteur@gmail.com <tymensteur@gmail.com@09e49e98-eaa8-4320-d9ac-a419819e7ac0>
2011-02-07 14:13:09 +0000

    Added the finalized Startdocument.doc and android_experiment_02

--- trunk/code/android/Android_Experiment_02/src/org/me/android_experiment_02/Main.java
+++ trunk/code/android/Android_Experiment_02/src/org/me/android_experiment_02/Main.java
@@ -1,15 +1,15 @@
-package org.me.android_experiment_01;
+package org.me.android_experiment_02;
 
 import android.app.Activity;
 import android.os.Bundle;
 
-public class Androway extends Activity
+public class Main extends Activity
 {
     @Override
     public void onCreate(Bundle icicle)
     {
         super.onCreate(icicle);
-        
+
         // The content of our screen.
         setContentView(R.layout.main);
     }
