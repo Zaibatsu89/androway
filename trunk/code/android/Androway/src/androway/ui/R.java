4637c119ec5efb8d917072fb1a29c89c4d2e2634
tymensteur@gmail.com <tymensteur@gmail.com@09e49e98-eaa8-4320-d9ac-a419819e7ac0>
2011-02-17 11:58:38 +0000

    Added the most recent layout to the new code structure. The direction part is now fully in. Also some experimental code is inserted for rotating the inclination segway body.

--- trunk/code/android/Androway/src/androway/ui/R.java
+++ trunk/code/android/Androway/src/androway/ui/R.java
@@ -43,42 +43,50 @@ public final class R {
         public static final int btn_strip_trans_right_normal=0x7f02000e;
         public static final int btn_strip_trans_right_pressed=0x7f02000f;
         public static final int btn_strip_trans_right_selected=0x7f020010;
-        public static final int inclination_bg=0x7f020011;
-        public static final int launcher_icon=0x7f020012;
-        public static final int log_web_icon=0x7f020013;
-        public static final int quickcontact_arrow_down=0x7f020014;
-        public static final int quickcontact_arrow_up=0x7f020015;
-        public static final int quickcontact_bottom_frame=0x7f020016;
-        public static final int quickcontact_disambig_bottom_bg=0x7f020017;
-        public static final int quickcontact_disambig_divider=0x7f020018;
-        public static final int quickcontact_drop_shadow=0x7f020019;
-        public static final int quickcontact_photo_frame=0x7f02001a;
-        public static final int quickcontact_slider_background=0x7f02001b;
-        public static final int quickcontact_slider_btn=0x7f02001c;
-        public static final int quickcontact_slider_btn_normal=0x7f02001d;
-        public static final int quickcontact_slider_btn_on=0x7f02001e;
-        public static final int quickcontact_slider_btn_pressed=0x7f02001f;
-        public static final int quickcontact_slider_btn_selected=0x7f020020;
-        public static final int quickcontact_slider_grip_left=0x7f020021;
-        public static final int quickcontact_slider_grip_right=0x7f020022;
-        public static final int quickcontact_top_frame=0x7f020023;
-        public static final int settings_icon=0x7f020024;
-        public static final int title_bar_bg=0x7f020025;
-        public static final int title_text=0x7f020026;
+        public static final int dir_arrow_left=0x7f020011;
+        public static final int dir_arrow_right=0x7f020012;
+        public static final int dir_arrow_straight=0x7f020013;
+        public static final int direction_bg=0x7f020014;
+        public static final int inclination_bg=0x7f020015;
+        public static final int launcher_icon=0x7f020016;
+        public static final int lego_wheel=0x7f020017;
+        public static final int log_web_icon=0x7f020018;
+        public static final int quickcontact_arrow_down=0x7f020019;
+        public static final int quickcontact_arrow_up=0x7f02001a;
+        public static final int quickcontact_bottom_frame=0x7f02001b;
+        public static final int quickcontact_disambig_bottom_bg=0x7f02001c;
+        public static final int quickcontact_disambig_divider=0x7f02001d;
+        public static final int quickcontact_drop_shadow=0x7f02001e;
+        public static final int quickcontact_photo_frame=0x7f02001f;
+        public static final int quickcontact_slider_background=0x7f020020;
+        public static final int quickcontact_slider_btn=0x7f020021;
+        public static final int quickcontact_slider_btn_normal=0x7f020022;
+        public static final int quickcontact_slider_btn_on=0x7f020023;
+        public static final int quickcontact_slider_btn_pressed=0x7f020024;
+        public static final int quickcontact_slider_btn_selected=0x7f020025;
+        public static final int quickcontact_slider_grip_left=0x7f020026;
+        public static final int quickcontact_slider_grip_right=0x7f020027;
+        public static final int quickcontact_top_frame=0x7f020028;
+        public static final int segway_body_direction=0x7f020029;
+        public static final int segway_body_inclination=0x7f02002a;
+        public static final int settings_icon=0x7f02002b;
+        public static final int title_bar_bg=0x7f02002c;
+        public static final int title_text=0x7f02002d;
     }
     public static final class id {
-        public static final int arrow_down=0x7f05000b;
-        public static final int arrow_up=0x7f050007;
-        public static final int battery_button=0x7f050002;
-        public static final int bluetooth_button=0x7f050003;
-        public static final int footer=0x7f05000a;
-        public static final int header2=0x7f050006;
-        public static final int icon=0x7f050004;
-        public static final int log_web_button=0x7f050001;
-        public static final int scroll=0x7f050008;
-        public static final int title=0x7f050005;
-        public static final int tracks=0x7f050009;
-        public static final int window_tint=0x7f050000;
+        public static final int arrow_down=0x7f070009;
+        public static final int arrow_up=0x7f070005;
+        public static final int battery_button=0x7f07000b;
+        public static final int bluetooth_button=0x7f07000c;
+        public static final int footer=0x7f070008;
+        public static final int header2=0x7f070004;
+        public static final int icon=0x7f070000;
+        public static final int log_web_button=0x7f07000a;
+        public static final int scroll=0x7f070006;
+        public static final int segway_body=0x7f070002;
+        public static final int title=0x7f070001;
+        public static final int tracks=0x7f070007;
+        public static final int window_tint=0x7f070003;
     }
     public static final class layout {
         public static final int action_item=0x7f030000;
@@ -92,28 +100,28 @@ public final class R {
     public static final class string {
         /**  Exception messages 
          */
-        public static final int MaxPoolSizeReachedException=0x7f060002;
-        public static final int app_label=0x7f060001;
-        public static final int app_name=0x7f060000;
+        public static final int MaxPoolSizeReachedException=0x7f050002;
+        public static final int app_label=0x7f050001;
+        public static final int app_name=0x7f050000;
     }
     public static final class style {
-        public static final int Animations=0x7f070005;
+        public static final int Animations=0x7f060005;
         /**  PopDownMenu 
          */
-        public static final int Animations_PopDownMenu=0x7f070006;
-        public static final int Animations_PopDownMenu_Center=0x7f070009;
-        public static final int Animations_PopDownMenu_Left=0x7f070007;
-        public static final int Animations_PopDownMenu_Right=0x7f070008;
+        public static final int Animations_PopDownMenu=0x7f060006;
+        public static final int Animations_PopDownMenu_Center=0x7f060009;
+        public static final int Animations_PopDownMenu_Left=0x7f060007;
+        public static final int Animations_PopDownMenu_Right=0x7f060008;
         /**  PopUpMenu 
          */
-        public static final int Animations_PopUpMenu=0x7f07000a;
-        public static final int Animations_PopUpMenu_Center=0x7f07000d;
-        public static final int Animations_PopUpMenu_Left=0x7f07000b;
-        public static final int Animations_PopUpMenu_Right=0x7f07000c;
-        public static final int StatusBarLeft=0x7f070002;
-        public static final int StatusBarMiddle=0x7f070003;
-        public static final int StatusBarRight=0x7f070004;
-        public static final int TitleBar=0x7f070001;
-        public static final int WindowTint=0x7f070000;
+        public static final int Animations_PopUpMenu=0x7f06000a;
+        public static final int Animations_PopUpMenu_Center=0x7f06000d;
+        public static final int Animations_PopUpMenu_Left=0x7f06000b;
+        public static final int Animations_PopUpMenu_Right=0x7f06000c;
+        public static final int StatusBarLeft=0x7f060002;
+        public static final int StatusBarMiddle=0x7f060003;
+        public static final int StatusBarRight=0x7f060004;
+        public static final int TitleBar=0x7f060001;
+        public static final int WindowTint=0x7f060000;
     }
 }
