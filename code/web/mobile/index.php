61a97edf4abfe3e032b12d5d2ada6517eebcd415
rinsecramer@gmail.com <rinsecramer@gmail.com@09e49e98-eaa8-4320-d9ac-a419819e7ac0>
2011-04-07 09:57:11 +0000

    Updated mobile web application.

---
+++ code/web/mobile/index.php
@@ -0,0 +1,28 @@
+<?php
+	require_once("init.php");
+?>
+<html>
+	<head>
+		<style type="text/css">
+			html
+			{
+				padding: 0;
+				margin: 0;
+			}
+			
+			#header
+			{
+				width: 100%;
+				height: 100px;
+				text-align: center;
+				background-color: #666;
+				color: #FFF;
+			}
+		</style>
+	</head>
+	<body>
+		<div id="header">
+			Mobiele pagina
+		</div>
+	</body>
+</html>
