b2185375fde4474b669d31d2c1aabcaa2075fd6d
tymensteur@gmail.com <tymensteur@gmail.com@09e49e98-eaa8-4320-d9ac-a419819e7ac0>
2011-04-28 09:17:49 +0000

    Added BluetoothCommunication (in class).rar
    Where the embedded bluetooth class is almost perfect.

--- trunk/code/embedded/BluetoothExperiment/BluetoothExperiment.pde
+++ trunk/code/embedded/BluetoothExperiment/BluetoothExperiment.pde
@@ -2,29 +2,43 @@
 #include "Connection.h"
 
 #define BT_BAUDRATE 9600
-#define PIN_BT_RX 4
-#define PIN_BT_TX 3
-#define PIN_BT_RST 5
-
-#define ledPin 13
+#define PIN_BT_DCD 1
+#define PIN_BT_TX 2
+#define PIN_BT_RX 3
+#define PIN_BT_RST 4
+#define PIN_LED 13
 
 // Set up a new software serial port
 NewSoftSerial btSerial(PIN_BT_RX, PIN_BT_TX);
 
 // Create a new (bt) connection with the desired settings
-Connection connection = Connection(BT_BAUDRATE, PIN_BT_RX, PIN_BT_TX, PIN_BT_RST);
+Connection connection = Connection(BT_BAUDRATE, PIN_BT_RX, PIN_BT_TX, PIN_BT_RST, PIN_BT_DCD, PIN_LED);
 
 void setup()
 {
-  pinMode(ledPin,OUTPUT);
-  digitalWrite(ledPin, LOW);
+  pinMode(PIN_LED, OUTPUT);
+  // Open (start) the serial port
+  btSerial.begin(BT_BAUDRATE);
   
   Serial.begin(9600);
   
   // Initialize the (bt) connection, passing the created SoftwareSerial and the desired device name
   connection.init(btSerial, "Androway");
+  
+  // Attach an interrupt to the RX pin of the bluetooth module
+  // Interrupt 0 = pin 2
+  // Interrupt 1 = pin 3. Used for checking when there is new data.
+  attachInterrupt(1, readData, RISING);
 }
 
+// The main program loop
 void loop()
 {
+  // Trigger the loop for the connection
+  connection.loop(btSerial);
+}
+
+void readData()
+{
+  connection.receiveData(btSerial);
 }
--- trunk/code/embedded/BluetoothExperiment/Connection.h
+++ trunk/code/embedded/BluetoothExperiment/Connection.h
@@ -7,18 +7,38 @@
 
 #include <WProgram.h>
 
+#define BUFFER_LENGTH 100              // The length of the receive buffer
+#define VERIFICATION_STRING_LENGTH 13  // The length of the verification string
+#define RECEIVED_DATA_LENGTH 30        // The length of the data array to receive
+
 class Connection
 {
   public:
-    Connection(int baudRate, int rxPin, int txPin, int rstPin);  // The class constructor
+    // The class constructor
+    Connection(int baudRate, int rxPin, int txPin, int rstPin, int dcdPin, int ledPin);
     void init(NewSoftSerial mySerial, String deviceName);
-    boolean receiveData(NewSoftSerial mySerial);
+    void loop(NewSoftSerial mySerial);
+    void receiveData(NewSoftSerial mySerial);
+    void processData();
+    void clearBuffer();
 
   private:
-    int _baudRate;
-    int _rxPin;
-    int _txPin;
-    int _rstPin;
+    char* verificationString[VERIFICATION_STRING_LENGTH];  // The verification string for the device. Each message should start with this for it to be valid.
+    char messageSeparator;                                // The separator used between different messages
+    char valueSeparator;                                        // The separator used between different values in each message
+    int maxMessageReceiveTime;                            // We should receive a message within this time. If it takes longer that this maximum, something went wrong.
+    
+    char* receiveBuffer[BUFFER_LENGTH];
+    int receiveCounter;
+    long lastReceivedMessage;  // The last time - in millis() - that we received a message from the Android device.
+    char* receivedData[RECEIVED_DATA_LENGTH];
+    
+    uint8_t _baudRate;
+    uint8_t _rxPin;
+    uint8_t _txPin;
+    uint8_t _rstPin;
+    uint8_t _dcdPin;
+    uint8_t _ledPin;
 };
     
 #endif
