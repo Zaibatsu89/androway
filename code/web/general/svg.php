a561c2c8f189c2d2bbdc224041f5a44c2dac55b5
rinsecramer@gmail.com <rinsecramer@gmail.com@09e49e98-eaa8-4320-d9ac-a419819e7ac0>
2011-05-10 13:33:48 +0000

    Updated svg.php. All points together stay in the svg frame, and are always visible. Automatically zooming in doesn't work yet.

--- code/web/general/svg.php
+++ code/web/general/svg.php
@@ -14,16 +14,19 @@
 <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 <title>Androway SVG development</title>
 <link rel="stylesheet" href="styles/svg.css" />
-<style type="text/css">
-@import "styles/jquery.svg.css";
+<link rel="stylesheet" href="styles/jquery.svg.css" />
 
-#svg_frame { height: 500px; border: 1px solid; }
-</style>
 <script type="text/javascript" src="scripts/lib/jquery.1.5.1.min.js"></script>
 <script type="text/javascript" src="scripts/lib/jquery.svg.pack.js"></script>
 <script type="text/javascript" src="scripts/lib/jquery.svganim.pack.js"></script>
 <script type="text/javascript">
-$(function() {
+$(function()
+{
+	var svgFramePadding = 10;
+	var browserWidth = $(window).width() - 2 * svgFramePadding;
+	var browserHeight = Math.round(browserWidth / 16 * 5) - 2 * svgFramePadding;
+	
+	$('#svg_frame').width(browserWidth).height(browserHeight).css('margin', svgFramePadding + 'px');
 	$('#svg_frame').svg({onLoad: init});
 	$('button').click(drawCommand);
 });
@@ -42,7 +45,7 @@ var strMove8 = '│ 40, -60';
 
 var idTextPosition = 'textPosition';
 var idTextHeading = 'textHeading';
-var idCircleGroup = 'all';
+var idAll = 'all';
 var idPath = 'path';
 var idCircleA = 'circleA';
 var idCircleB = 'circleB';
@@ -64,8 +67,19 @@ var path;
 var xFlag = false;
 var yFlag = false;
 
+var svgFramePadding = 10;
+var browserWidth = $(window).width() - 2 * svgFramePadding;
+var browserHeight = Math.round(browserWidth / 16 * 5) - 2 * svgFramePadding;
+
+var xMin = 0;
+var xMax = 0;
+var yMin = 0;
+var yMax = 0;
+
+var scale = 1;
+
 function init(svg)
-{	
+{
 	$('#add_segway').text(strAdd);
 	$('#move_1').text(strMove1);
 	$('#move_2').text(strMove2);
@@ -75,7 +89,7 @@ function init(svg)
 	$('#move_6').text(strMove6);
 	$('#move_7').text(strMove7);
 	$('#move_8').text(strMove8);
-	$('#show_svg').text('Show SVG');
+	$('#show_svg').text('SVG');
 	
 	var defs = svg.defs();
 	var ptn = svg.pattern(defs, 'grid', 0, 0, 20, 20, {patternUnits: 'userSpaceOnUse'});
@@ -85,8 +99,10 @@ function init(svg)
 	var rect4 = svg.rect(ptn, 0, 10, 10, 10, {fill: 'white'});
 	var rectFill = svg.rect(0, 0, 0, 0, {fill: 'url(#grid)', width: '100%', height: '100%'});
 	
-	var circleGroup = svg.group({id: idCircleGroup, transform: 'translate(0 0)'});
+	var all = svg.group({id: idAll, transform: 'translate(0 0) scale(' + scale + ')'});
+	
 	path = svg.createPath();
+	svg.path(all, path, {id: idPath, stroke: '#D90000', strokeWidth: 1});
 }
 
 function drawCommand()
@@ -125,21 +141,29 @@ function drawCommand()
 			break;
 	}
 	
-	displayValues();
+	checkBorders();
 	
-	moveCircleGroup();
+	displayValues();
 }
 
 function move(left, right)
 {	
 	var svg = $('#svg_frame').svg('get');
-	var circleGroup = svg.getElementById(idCircleGroup);
+	var all = svg.getElementById(idAll);
 	
-	var xBefore = 0;
-	var yBefore = 0;
+	var xBefore = $('#svg_frame').width() / 2;
+	var yBefore = $('#svg_frame').height() / 2;
 	
 	if (typeof(androwayCoordinates['center']) == 'undefined')
-		androwayCoordinates['center'] = {x: 0, y: 0};
+	{
+		var width = $('#svg_frame').width() / 2;
+		var height = $('#svg_frame').height() / 2;
+		
+		xMin = width;
+		yMin = height;
+		
+		androwayCoordinates['center'] = {x: width, y: height};
+	}
 	else
 	{
 		setHeading(left, right);
@@ -148,23 +172,40 @@ function move(left, right)
 		yBefore = androwayCoordinates['center'].y;
 		
 		androwayCoordinates['center'] = {x: androwayCoordinates['center'].x + getX(left, right), y: androwayCoordinates['center'].y + getY(left, right)};
+		
+		console.log('current x: ' + androwayCoordinates['center'].x);
+		console.log('current y: ' + androwayCoordinates['center'].y);
 	}
 	
 	var xAfter = androwayCoordinates['center'].x;
 	var yAfter = androwayCoordinates['center'].y;
 	
-	svg.circle(circleGroup, androwayCoordinates['center'].x, androwayCoordinates['center'].y, 3, {heading: heading % 360, fill: '#666', id: idCircleB});
+	svg.circle(all, androwayCoordinates['center'].x, androwayCoordinates['center'].y, 3, {heading: heading % 360, fill: '#666', id: idCircleB});
 	
-	svg.path(circleGroup, path.move(xBefore, yBefore).line(xAfter, yAfter).close(), {id: idPath, fill: 'black', stroke: '#D90000', strokeWidth: 1});
+	var p = svg.getElementById(idPath);
+	var move = path.move(xBefore, yBefore).line(xAfter, yAfter);
+	
+	svg.change(p, {d: move._path});
 	
 	if ($('#' + idCircleA).length <= 0)
 		
-		svg.circle(circleGroup, androwayCoordinates['center'].x, androwayCoordinates['center'].y, 4, {fill: 'black', id: idCircleA});
+		svg.circle(all, androwayCoordinates['center'].x, androwayCoordinates['center'].y, 4, {fill: 'black', id: idCircleA});
 	else
 	{
 		var circleA = svg.getElementById(idCircleA);
 		svg.change(circleA, {cx: androwayCoordinates['center'].x, cy: androwayCoordinates['center'].y});
 	}
+	
+	if (androwayCoordinates['center'].x < xMin)
+		xMin = androwayCoordinates['center'].x;
+	if (androwayCoordinates['center'].x > xMax)
+		xMax = androwayCoordinates['center'].x;
+	if (androwayCoordinates['center'].y < yMin)
+		yMin = androwayCoordinates['center'].y;
+	if (androwayCoordinates['center'].y > yMax)
+		yMax = androwayCoordinates['center'].y;
+		
+	console.log('xMin: ' + xMin, 'xMax: ' + xMax, 'yMin: ' + yMin, 'yMax: ' + yMax);
 }
 
 function setHeading(left, right)
@@ -189,11 +230,10 @@ function getY(left, right)
 	return -0.5 * (left + right) * Math.cos(heading * Math.PI / 180);
 }
 
-function moveCircleGroup()
+function moveAll()
 {
 	var svg = $('#svg_frame').svg('get');
-	
-	var circleGroup = svg.getElementById(idCircleGroup);
+	var all = svg.getElementById(idAll);
 	
 	var x = $('#svg_frame').width() / 2;
 	var y = $('#svg_frame').height() / 2;
@@ -207,25 +247,123 @@ function moveCircleGroup()
 	xD = xDiff - oldXDiff;
 	yD = yDiff - oldYDiff;
 	
-	svg.change(circleGroup, {transform: 'translate(' + xDiff + ' ' + yDiff + ')'});
+	svg.change(all, {transform: 'translate(' + xDiff + ' ' + yDiff + ') scale(' + scale + ')'});
 }
 
 function checkBorders()
 {	
+	var svg = $('#svg_frame').svg('get');
+	var all = svg.getElementById(idAll);
+	
+	var x = Math.abs(xMin - xMax);
+	var y = Math.abs(yMin - yMax);
+	
 	var width = $('#svg_frame').width();
 	var height = $('#svg_frame').height();
 	
 	if (typeof(androwayCoordinates['center']) != 'undefined')
 	{
-		if (androwayCoordinates['center'].x < 6)
-			androwayCoordinates['center'] = {x: 7.5, y: androwayCoordinates['center'].y};
-		if (androwayCoordinates['center'].x > width - 6)
-			androwayCoordinates['center'] = {x: width - 7.5, y: androwayCoordinates['center'].y};
-		if (androwayCoordinates['center'].y < 6)
-			androwayCoordinates['center'] = {x: androwayCoordinates['center'].x, y: 10};
-		if (androwayCoordinates['center'].y > height - 6)
-			androwayCoordinates['center'] = {x: androwayCoordinates['center'].x, y: height - 10};
+		// Zoom out
+		if (x > xMax)
+		{
+			console.log('x > xMax: ' + x + ' > ' + xMax);
+			scale = width / x;
+		}
+		if (y > yMax / 2)
+		{
+			console.log('y > (yMax / 2): ' + y + ' > ' + yMax / 2);
+			scale = height / (2 * y);
+		}
+		
+		// TODO: Zoom in
+		if (xMin < androwayCoordinates['center'].x < xMax)
+		{
+			console.log('xMin < current x < xMax: ' + xMin + ' < ' + androwayCoordinates['center'].x + ' < ' + xMax);
+			//scale = width / (xMin + androwayCoordinates['center'].x + xMax);
+		}
+		if (yMin < androwayCoordinates['center'].y < yMax)
+		{
+			console.log('yMin < current y < yMax: ' + yMin + ' < ' + androwayCoordinates['center'].y + ' < ' + yMax);
+			//scale = height / (yMin + androwayCoordinates['center'].y + yMax);
+		}
+		
+		if (scale < 0.2)
+			scale = 0.2;
+		if (scale > 20)
+			scale = 20;
+		
+		var scaleWidth = 0;
+		var scaleHeight = 0;
+		
+		if(scale != 1)
+		{
+			scaleWidth = (width - width * scale) / 2;
+			scaleHeight = (height - height * scale) / 2;
+		}
+		
+		var xHalf = $('#svg_frame').width() / 2;
+		var yHalf = $('#svg_frame').height() / 2;
+		
+		oldXDiff = xDiff;
+		oldYDiff = yDiff;
+		
+		xDiff = xHalf - androwayCoordinates['center'].x;
+		yDiff = yHalf - androwayCoordinates['center'].y;
+		
+		xDiff *= scale;
+		yDiff *= scale;
+		
+		svg.change(all, {transform: 'translate(' + (xDiff + scaleWidth) + ', ' + (yDiff + scaleHeight) + ') scale(' + scale + ')'});
+		//if (androwayCoordinates['center'].x < xMin)
+//		{
+//			console.log(androwayCoordinates['center'].x + ' < xMin');
+//			
+//			scale *= 0.99;
+//			svg.change(all, {transform: 'translate(' + x + ', ' + y + ') scale(' + scale + ')'});
+//			
+//			xMin = androwayCoordinates['center'].x;
+//		}
+//		if (androwayCoordinates['center'].x > xMax)
+//		{
+//			console.log(androwayCoordinates['center'].x + ' > xMax');
+//			
+//			scale *= 0.99;
+//			svg.change(all, {transform: 'translate(' + x + ', ' + y / 2 + ') scale(' + scale + ')'});
+//			
+//			xMax = androwayCoordinates['center'].x;
+//		}
+//		if (androwayCoordinates['center'].y < yMin)
+//		{
+//			console.log(androwayCoordinates['center'].y + ' < yMin');
+//			
+//			scale = height / Math.abs(yMin);
+//			
+//			if (scale > 1)
+//				scale = 1;
+//			
+//			x /= scale;
+//			y /= scale;
+//			
+//			console.log('xTransform: ' + x, 'yTransform: ' + y);
+//			
+//			svg.change(all, {transform: 'translate(' + x + ', ' + y + ') scale(' + scale + ')'});
+//			
+//			yMin = androwayCoordinates['center'].y;
+//		}
+//		if (androwayCoordinates['center'].y > yMax)
+//		{
+//			console.log(androwayCoordinates['center'].y + ' > yMax');
+//			
+//			scale *= 0.99;
+//			svg.change(all, {transform: 'translate(' + x + ', ' + y + ') scale(' + scale + ')'});
+//			
+//			yMax = androwayCoordinates['center'].y;
+//		}
 	}
+	
+	console.log('xTransform: ' + x, 'yTransform: ' + y);
+	console.log('scale: ' + scale);
+	console.log('--------------');
 }
 
 function displayValues()
@@ -236,8 +374,9 @@ function displayValues()
 	{
 		if ($('#' + idTextPosition).length <= 0)
 		{
-			svg.text(5, 18, 'Position: (' + androwayCoordinates['center'].x + ', ' + androwayCoordinates['center'].y + ')', {id: idTextPosition, style: 'font-family:Calibri;font-size:16px;'});
-			svg.text(5, 36, 'Heading: ' + heading % 360, {id: idTextHeading, style: 'font-family:Calibri;font-size:16px;'});
+			svg.text(5, 18, 'Dimensions: (' + $('#svg_frame').width() + ', ' + $('#svg_frame').height() + ')', {style: 'font-family:Calibri;font-size:16px;'});
+			svg.text(5, 36, 'Position: (' + androwayCoordinates['center'].x + ', ' + androwayCoordinates['center'].y + ')', {id: idTextPosition, style: 'font-family:Calibri;font-size:16px;'});
+			svg.text(5, 54, 'Heading: ' + heading % 360, {id: idTextHeading, style: 'font-family:Calibri;font-size:16px;'});
 		}
 		else
 		{
