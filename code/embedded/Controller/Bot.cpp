24bb330a464ea7d1eef059a4b138ef4f5a0dcec2
tymensteur@gmail.com <tymensteur@gmail.com@09e49e98-eaa8-4320-d9ac-a419819e7ac0>
2011-05-11 09:57:36 +0000

    Renamed Communication to Controller (so Communication is deleted and Controller is added)
    Bluetooth fully working including the Bot class

---
+++ code/embedded/Controller/Bot.cpp
@@ -0,0 +1,22 @@
+/*
+  Bot.cpp - Class for interacting with the physical balancing bot.
+ */
+
+#include "WProgram.h"
+#include "Bot.h"
+
+// Set the new data for the bot
+void Bot::setData(struct BotData botData)
+{
+  // Store the given BotData as the new data for the bot
+  _data = botData;
+
+  Serial.println("Set data:");
+  Serial.print(_data.drivingDirection);
+  Serial.print(", ");
+  Serial.print(_data.drivingSpeed);
+  Serial.print(", ");
+  Serial.print(_data.isOnHold, DEC);
+  Serial.println();
+};
+
---
+++ code/embedded/Controller/Bot.h
@@ -0,0 +1,39 @@
+/*
+  Boat.cpp - Class for interacting with the physical boat.
+*/
+
+#ifndef Bot_h
+#define Bot_h
+
+#include <WProgram.h>
+
+#define SENSOR_1_PIN 6  // Pin for our first sensor
+#define SENSOR_2_PIN 7  // Pin for our second sensor
+
+// The BotData object used for storing the data of the bot
+typedef struct BotData
+{
+  float drivingDirection;
+  int drivingSpeed;
+  boolean isOnHold;
+};
+
+class Bot
+{
+  private:
+    BotData _data;  // The private BotData _data for storing the data of the bot
+    
+  public:
+    // The constructor for the class, store the NewSoftSerial instance and the pin numbers in private variables.
+    Bot()
+    {
+      // Initialize some variables that are used by the class
+      BotData _data = {};
+      _data.isOnHold = true;  // The bot is set on hold by default
+    };
+
+    struct BotData getData(){ return _data; };  // Get the data currently used by the bot
+    void setData(struct BotData botData);       // Set the new data for the bot
+};
+
+#endif
