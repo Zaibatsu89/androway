2ad193b200d9219192ff0f051a16e72be49b4c60
tymensteur@gmail.com <tymensteur@gmail.com@09e49e98-eaa8-4320-d9ac-a419819e7ac0>
2011-06-21 10:12:41 +0000

    Commented

--- code/embedded/Controller/Bluetooth.h
+++ code/embedded/Controller/Bluetooth.h
@@ -1,50 +1,166 @@
-/*
-  Bluetooth.h - Class for handeling the bluetooth connection and the messages.
- */
-
 #ifndef Bluetooth_h
 #define Bluetooth_h
 
 #include <NewSoftSerial.h>
 #include <WProgram.h>
 
-#define BUFFER_LENGTH 100                   // The length of the receive buffer
-#define RECEIVED_DATA_LENGTH 30             // The length of the data array to receive
-#define MAX_MESSAGE_RECEIVE_TIME 10000      // We should receive a message within this time. If it takes longer that this maximum, something went wrong.
+/**
+ * The length of the receive buffer
+ */
+#define BUFFER_LENGTH 100
 
-#define MESSAGE_SEPARATOR '~'               // The separator used between the different messages
-#define VALUE_SEPARATOR ','                 // The separator used between different values in each message
-#define VERIFICATION_STRING "$ANDROWAY001"  // The verification string used for verifying the incoming messages
+/**
+ * The length of the data array to receive
+ */
+#define RECEIVED_DATA_LENGTH 30
 
-// Check if the given value is a digit (number)
+/**
+ * We should receive a message within this time. If it takes longer that this maximum, something went wrong.
+ */
+#define MAX_MESSAGE_RECEIVE_TIME 10000
+
+/**
+ * The separator used between the different messages
+ */
+#define MESSAGE_SEPARATOR '~'
+
+/**
+ * The separator used between different values in each message
+ */
+#define VALUE_SEPARATOR ','
+
+/**
+ * The verification string used for verifying the incoming messages
+ */
+#define VERIFICATION_STRING "$ANDROWAY001"
+
+/**
+ * A function for checking if the given value is a digit (number)
+ */
 #define isDigit(n) (n >= '0' && n <= '9')
 
 extern "C"
 {
-  // Define a type for the callback function. Takes a char array as parameter
+  /**
+   * Object used for callback functions.
+   * @author Tymen Steur
+   * @since 06-06-2011
+   * @version 0.3
+   * @param The data to pass to the callback function
+   */
   typedef void (*BluetoothCallback)(char data[]);
 }
 
+/**
+ * The Bluetooth class for handeling a bluetooth connection and the sending/receiving of messages.
+ * @author Tymen Steur
+ * @since 06-06-2011
+ * @version 0.3
+ */
 class Bluetooth
 {
   private:
+    /**
+     * The new soft serial for the serial connection with the bluetooth adaptor
+     */
     NewSoftSerial _mySerial;
+    
+    /**
+     * The callback function to trigger when data was received
+     */
     BluetoothCallback _receiveCallback;
+    
+    /**
+     * The callback function to trigger when a message has to be sent (based on timer)
+     */
     BluetoothCallback _sendCallback;
+    
+    /**
+     * The rx (receive) pin number for the bluetooth adaptor
+     */
     byte _rxPin;
+    
+    /**
+     * The tx (transmit) pin number for the bluetooth adaptor
+     */
     byte _txPin;
+    
+    /**
+     * The rst (reset) pin number for the bluetooth adaptor
+     */
     byte _rstPin;
+    
+    /**
+     * The dcd (data carrier detect) pin number for the bluetooth adaptor
+     */
     byte _dcdPin;
+    
+    /**
+     * The pin number for the led
+     */
     byte _ledPin;
+    
+    /**
+     * Used for counting the number of characters received
+     */
     byte _receiveCounter;
+    
+    /**
+     * Used to hold the time we last received a message in millis
+     */
     unsigned long _lastReceivedMessage;
+    
+    /**
+     * Used to hold the time we last sent a message in millis
+     */
     unsigned long _lastSentMessage;
+    
+    /**
+     * The interval for sending messages to the remote device
+     */
     unsigned long _sendMessageInterval;
+    
+    /**
+     * The buffer for holding the received characters
+     */
     char _receiveBuffer[BUFFER_LENGTH];
+    
+    /**
+     * The buffer for holding the received data
+     */
     char _receivedData[RECEIVED_DATA_LENGTH];
+    
+    /**
+     * Clear the receive buffer
+     */
+    void clearBuffer();
+    
+    /**
+     * Handle the received character
+     * @param value  The received character
+     */
+    void handleChar(char value);
+    
+    /**
+     * Handle the received message
+     */
+    void handleMessage();
+    
+    /**
+     * Verify the received message based on the verification string
+     * @returns Wheter the message was correct or not
+     */
+    boolean verifyMessage();
       
   public:
-    // The constructor for the class, store the pin numbers and initialize the bluetooth software serial connection
+    /**
+     * The constructor for the Bluetooth class. Initializes the (software) serial connection.
+     * @param rxPin   The pin number for the rx (receive) pin
+     * @param txPin   The pin number for the tx (transmit) pin
+     * @param rstPin  The pin number for the rst (reset) pin
+     * @param dcdPin  The pin number for the dcd (data carrier detect) pin
+     * @param ledPin  The pin number for the led
+     */
     Bluetooth(byte rxPin, byte txPin, byte rstPin, byte dcdPin, byte ledPin) : _mySerial(rxPin, txPin)
     {
       // Store the given settings in the private variables
@@ -65,18 +181,57 @@ class Bluetooth
       _sendMessageInterval = 1500;  // The default interval for sending a message back (in ms)
     };
     
-    void begin(int baudrate, char name[], char password[]);      // Function for starting the bluetooth class and connection/module
-    void loop();                                                 // The main loop for the bluetooth class
-    void attachReceiveCallback(BluetoothCallback callback);      // Attach the given function as callback function for received data messages
-    void attachSendCallback(BluetoothCallback callback, unsigned long interval);  // Attach the given function as callback function for when it is time to send a new message
-    void clearBuffer();                                          // Function that clears the receive buffer
-    void receiveData();                                          // Function that handles the receiving of the bluetooth data
-    void sendData(char* message);                                // Function for sending data messages to the connected device
-    void handleChar(char value);                                 // Handle the received character
-    void handleMessage();                                        // Handle the received message
-    boolean verifyMessage();                                     // Verify the received message based on the verification string
-    char* floatToString(float floatVal);                         // Convert the given float to a string
-    char* appendString(char baseString[], char appendString[]);  // Append the given append string to the base string and return the new string
+    /**
+     * Starting the bluetooth connection/module
+     * @param baudrate  The baudrate to start the bluetooh connection with (CURRENTLY DOESN'T WORK WITH ANY OTHER VALUE THEN 9600)
+     * @param name      The name for the bluetooth device
+     * @param password  The pin/password to use for the connection
+     */
+    void begin(int baudrate, char name[], char password[]);
+    
+    /**
+     * The main loop for the bluetooth class
+     */
+    void loop();
+    
+    /**
+     * Attaches the given function as callback for received data messages
+     * @param callback  The callback function
+     */
+    void attachReceiveCallback(BluetoothCallback callback);
+    
+    /**
+     * Attaches the given function as callback for when it is time to send a new message
+     * @param callback  The callback function
+     * @param interval  The time interval for sending messages in millis
+     */
+    void attachSendCallback(BluetoothCallback callback, unsigned long interval);
+    
+    /**
+     * Handle the receiving of the bluetooth data
+     */
+    void receiveData();
+    
+    /**
+     * Send the given message to the connected device
+     * @param message  The data message to send
+     */
+    void sendData(char* message);    
+    
+    /**
+     * Convert the given float to a string
+     * @param floatVal  The float to convert
+     * @return The string containing the float value
+     */
+    char* floatToString(float floatVal);
+    
+    /**
+     * Append the given append string to the base string and return the new string
+     * @param baseString    The base string
+     * @param appendString  The string to append
+     * @return The merged string
+     */
+    char* appendString(char baseString[], char appendString[]);
 };
 
 #endif
